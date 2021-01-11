f13f334a48e2f89403d78173579b9c67d1ae451e
luozhouyang <zhouyang.luo@gmail.com>
2021-01-11 15:23:51 +0800

    Release v0.0.3

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="keras_crf",
-    version="0.0.2",
+    version="0.0.3",
     description="A more elegant and convenient CRF built on tensorflow-addons.",
     long_description=long_description,
     long_description_content_type="text/markdown",
