f97d64d731767f840b26f41ac7557e43867e7df4
luozhouyang <zhouyang.luo@gmail.com>
2021-01-12 16:53:56 +0800

    Add seqeval callback

---
+++ keras_crf/callbacks.py
@@ -0,0 +1,50 @@
+import numpy as np
+import tensorflow as tf
+from seqeval.metrics import classification_report
+
+from .dataset import read_conll_format_file
+
+
+class EvaluateCallback(tf.keras.callbacks.Callback):
+
+    def __init__(self, token_mapper, label_mapper, valid_files, batch_size=32, sequence_maxlen=512):
+        self.token_mapper = token_mapper
+        self.laber_mapper = label_mapper
+        self.batch_size = batch_size
+        self.sequence_maxlen = sequence_maxlen
+
+        self.valid_features, self.valid_labels = [], []
+        features, labels = read_conll_format_file(valid_files)
+        for f, l in zip(features, labels):
+            if len(f) > sequence_maxlen:
+                continue
+            f = self.token_mapper.encode(f)
+            # l = self.laber_mapper.encode(l)
+            self.valid_features.append(f)
+            self.valid_labels.append(l)
+
+    def _build_dataset(self, features, batch_size=32):
+        maxlen = max([len(x) for x in features])
+        features = [x + [0] * (maxlen - len(x)) for x in features]
+        x = tf.constant(features)
+        x = tf.data.Dataset.from_tensor_slices(x)
+        x = x.batch(batch_size)
+        return x
+
+    def on_epoch_end(self, epoch, logs=None):
+        dataset = self._build_dataset(self.valid_features, batch_size=self.batch_size)
+        # shape: [batch_size, max_seq_len, num_class]
+        predictions = self.model.predict(dataset)
+        # shape: [batch_size, max_seq_len]
+        predictions = np.argmax(predictions, axis=-1)
+        y_true = self.valid_labels
+        y_pred = []
+        for _pred, _true in zip(predictions, y_true):
+            # remove paddings
+            _pred = _pred[:len(_true)]
+            tags = self.laber_mapper.decode(_pred.tolist())
+            y_pred.append(tags)
+        report = classification_report(y_true, y_pred)
+        print()
+        print('reports of epoch {}\n'.format(epoch))
+        print(report)
--- tests/model_train_test.py
+++ tests/model_train_test.py
@@ -1,24 +1,30 @@
+import os
 import unittest
 
+import numpy as np
 import tensorflow as tf
 import tensorflow_addons as tfa
+from keras_crf.callbacks import EvaluateCallback
 from keras_crf.crf import CRF, CRFAccuracy, CRFLoss
 from keras_crf.dataset import ChinaPeopleDailyBuilder, LabelMapper, TokenMapper
 
 token_mapper = TokenMapper('data/vocab.txt')
 label_mapper = LabelMapper()
 
+DATADIR = 'data/china-people-daily-ner-corpus'
+
 
 class ModelTrainTest(unittest.TestCase):
 
     def _build_inputs(self):
-        builder = ChinaPeopleDailyBuilder('data/vocab.txt')
-        train_dataset = builder.build_valid_dataset('data/china-people-daily-ner-corpus/example.train')
-        x = ['相', '比', '之', '下', '，', '青', '岛', '海', '牛', '队', '和', '广', '州', '松',
-             '木', '队', '的', '雨', '中', '之', '战', '虽', '然', '也', '是', '0', ':', '0']
-        tokens = token_mapper.encode(x)
-        predict_inputs = tf.constant(tokens, shape=(1, len(tokens)), dtype=tf.int32)
-        return train_dataset, predict_inputs
+        builder = ChinaPeopleDailyBuilder(token_mapper, label_mapper)
+        train_dataset = builder.build_valid_dataset(os.path.join(DATADIR, 'example.train'))
+        # x = ['相', '比', '之', '下', '，', '青', '岛', '海', '牛', '队', '和', '广', '州', '松',
+        #      '木', '队', '的', '雨', '中', '之', '战', '虽', '然', '也', '是', '0', ':', '0']
+        # tokens = token_mapper.encode(x)
+        # predict_inputs = tf.constant(tokens, shape=(1, len(tokens)), dtype=tf.int32)
+        predict_dataset, labels = builder.build_test_dataset(os.path.join(DATADIR, 'example.test'), with_labels=True)
+        return train_dataset, predict_dataset, labels
 
     def _build_model(self):
         sequence_input = tf.keras.layers.Input(shape=(None,), dtype=tf.int32, name='sequence_input')
@@ -38,13 +44,21 @@ class ModelTrainTest(unittest.TestCase):
 
     def test_train_model(self):
         model = self._build_model()
-        dataset, predict_dataset = self._build_inputs()
-        model.fit(dataset, steps_per_epoch=200)
+        dataset, predict_dataset, labels = self._build_inputs()
+        model.fit(
+            dataset,
+            steps_per_epoch=200,
+            callbacks=[
+                EvaluateCallback(token_mapper, label_mapper, os.path.join(DATADIR, 'example.dev')),
+            ])
 
-        preds = model.predict(predict_dataset)
-        for p in preds:
-            print(p)
-            print(label_mapper.decode(p))
+        preds = model.predict(predict_dataset, steps=1)
+        for idx, p in enumerate(preds):
+            # print(p)
+            example = np.argmax(p, axis=-1).tolist()[:len(labels[idx])]
+            print(labels[idx])
+            print(label_mapper.decode(example))
+            print()
 
 
 if __name__ == "__main__":
