3e5507a5bbba9609122b360e736674e14a61df47
luozhouyang <zhouyang.luo@gmail.com>
2021-01-19 20:14:46 +0800

    Release v0.1.1

--- keras_crf/__init__.py
+++ keras_crf/__init__.py
@@ -1,4 +1,4 @@
 from .crf import CRF
 
 __name__ = 'keras_crf'
-__version__ = '0.1.0'
+__version__ = '0.1.1'
--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="keras_crf",
-    version="0.1.0",
+    version="0.1.1",
     description="A more elegant and convenient CRF built on tensorflow-addons.",
     long_description=long_description,
     long_description_content_type="text/markdown",
