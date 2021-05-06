f5b80f69119325826ff7e325c5c7337caa1d5728
luozhouyang <zhouyang.luo@gmail.com>
2021-05-06 21:18:06 +0800

    Update imports

--- keras_crf/__init__.py
+++ keras_crf/__init__.py
@@ -1,4 +1,7 @@
+from .callbacks import EvaluateCallback
 from .crf_model import CRFModel
+from .dataset import (ChinaPeopleDailyBuilder, DatasetBuilder, LabelMapper,
+                      TokenMapper)
 
 __name__ = 'keras_crf'
 __version__ = '0.1.1'
