d19f47a2c4e5df539bea285a5cb24c5e7e3e4fbb
luozhouyang <zhouyang.luo@gmail.com>
2021-08-02 17:48:58 +0800

    refactoring CRF model

--- keras_crf/crf_model.py
+++ keras_crf/crf_model.py
@@ -14,74 +14,64 @@ def _unpack_data(data):
 class CRFModel(tf.keras.Model):
 
     def __init__(self,
-                 model,
+                 model: tf.keras.Model,
                  units: int,
                  chain_initializer="orthogonal",
                  use_boundary: bool = True,
                  boundary_initializer="zeros",
                  use_kernel: bool = True,
                  **kwargs):
-        super().__init__()
-        self.base_model = model
-        self.units = units
-        self.crf = tfa.layers.CRF(
+        # build functional model
+        crf = tfa.layers.CRF(
             units=units,
             chain_initializer=chain_initializer,
             use_boundary=use_boundary,
             boundary_initializer=boundary_initializer,
             use_kernel=use_kernel,
             **kwargs)
-        self.sequence_length = None
-
-    def call(self, inputs, training=None, mask=None):
-        """CRF endpoint forward pass.
-        
-        Args:
-            inputs: Tensor, shape (batch_size, seq_len, hidden_size)
-
-        Returns:
-            potentials: Tensor, shape (batch_size, seq_len, units), in train phase
-            sequence: Tensor, shape (batch_size, seq_len, units), in predict phase
-        """
-        outputs = self.base_model(inputs)
-        sequence, potentials, self.sequence_length, _ = self.crf(outputs)
-        if training:
-            return potentials
-        return tf.cast(tf.one_hot(sequence, self.units), dtype=self.dtype)
-
-    def _compute_loss(self, y_true, y_pred, sample_weight=None):
-        crf_loss = -tfa.text.crf_log_likelihood(y_pred, y_true, self.sequence_length, self.crf.chain_kernel)[0]
-        if sample_weight is not None:
-            crf_loss = crf_loss * sample_weight
-        crf_loss = tf.reduce_mean(crf_loss)
-        return crf_loss
+        # take model's first output passed to CRF layer
+        decode_sequence, potentials, sequence_length, kernel = crf(inputs=model.outputs[0])
+        # set name for outputs
+        decode_sequence = tf.keras.layers.Lambda(lambda x: x, name='decode_sequence')(decode_sequence)
+        potentials = tf.keras.layers.Lambda(lambda x: x, name='potentials')(potentials)
+        sequence_length = tf.keras.layers.Lambda(lambda x: x, name='sequence_length')(sequence_length)
+        kernel = tf.keras.layers.Lambda(lambda x: x, name='kernel')(kernel)
+        super().__init__(
+            inputs=model.inputs,
+            outputs=[decode_sequence, potentials, sequence_length, kernel],
+            **kwargs)
+        self.crf = crf
 
     def train_step(self, data):
         x, y, sample_weight = _unpack_data(data)
         with tf.GradientTape() as tape:
-            # prediction is potentials
-            prediction = self(x, training=True)
-            crf_loss = self._compute_loss(y, prediction, sample_weight=sample_weight)
-            total_loss = crf_loss + sum(self.losses)
-        gradients = tape.gradient(total_loss, self.trainable_variables)
+            decode_sequence, potentials, sequence_length, kernel = self(x, training=True)
+            crf_loss = -tfa.text.crf_log_likelihood(potentials, y, sequence_length, kernel)[0]
+            if sample_weight is not None:
+                crf_loss = crf_loss * sample_weight
+            crf_loss = tf.reduce_mean(crf_loss)
+            loss = crf_loss + sum(self.losses)
+        gradients = tape.gradient(loss, self.trainable_variables)
         self.optimizer.apply_gradients(zip(gradients, self.trainable_variables))
 
         # Update metrics (includes the metric that tracks the loss)
-        y_pred = tf.argmax(prediction, axis=-1)
-        self.compiled_metrics.update_state(y, y_pred)
+        self.compiled_metrics.update_state(y, potentials)
         # Return a dict mapping metric names to current value
-        results = {'loss': total_loss, 'crf_loss': crf_loss}
-        results.update({m.name: m.result() for m in self.metrics})
+        results = {m.name: m.result() for m in self.metrics}
+        results.update({'loss': loss})
         return results
 
     def test_step(self, data):
         x, y, sample_weight = _unpack_data(data)
-        # prediction is viterbi decoded sequence
-        prediction = self(x, training=False)
-        crf_loss = self._compute_loss(y, prediction, sample_weight=sample_weight)
-
+        decode_sequence, potentials, sequence_length, kernel = self(x, training=False)
+        crf_loss = -tfa.text.crf_log_likelihood(potentials, y, sequence_length, kernel)[0]
+        if sample_weight is not None:
+            crf_loss = crf_loss * sample_weight
+        crf_loss = tf.reduce_mean(crf_loss)
+        loss = crf_loss + sum(self.losses)
         # Update metrics (includes the metric that tracks the loss)
-        self.compiled_metrics.update_state(y, tf.argmax(prediction, axis=-1))
-        results = {'loss': crf_loss + sum(self.losses), 'crf_loss': crf_loss}
-        results.update({m.name: m.result() for m in self.metrics})
+        self.compiled_metrics.update_state(y, potentials)
+        # Return a dict mapping metric names to current value
+        results = {m.name: m.result() for m in self.metrics}
+        results.update({'loss': loss})
         return results
--- tests/crf_model_test.py
+++ tests/crf_model_test.py
@@ -3,26 +3,11 @@ import unittest
 
 import numpy as np
 import tensorflow as tf
-from keras_crf.callbacks import EvaluateCallback
+
 from keras_crf.crf_model import CRFModel
-from keras_crf.dataset import ChinaPeopleDailyBuilder, LabelMapper, TokenMapper
 
-token_mapper = TokenMapper('data/vocab.txt')
-label_mapper = LabelMapper()
-
-DATADIR = 'data/china-people-daily-ner-corpus'
 
 class CRFModelTest(unittest.TestCase):
-
-    def _build_inputs(self):
-        builder = ChinaPeopleDailyBuilder(token_mapper, label_mapper)
-        train_dataset = builder.build_valid_dataset(os.path.join(DATADIR, 'example.train'))
-        # x = ['相', '比', '之', '下', '，', '青', '岛', '海', '牛', '队', '和', '广', '州', '松',
-        #      '木', '队', '的', '雨', '中', '之', '战', '虽', '然', '也', '是', '0', ':', '0']
-        # tokens = token_mapper.encode(x)
-        # predict_inputs = tf.constant(tokens, shape=(1, len(tokens)), dtype=tf.int32)
-        predict_dataset, labels = builder.build_test_dataset(os.path.join(DATADIR, 'example.test'), with_labels=True)
-        return train_dataset, predict_dataset, labels
 
     def _build_model(self):
         sequence_input = tf.keras.layers.Input(shape=(None,), dtype=tf.int32, name='sequence_input')
@@ -30,7 +15,7 @@ class CRFModelTest(unittest.TestCase):
         outputs = tf.keras.layers.Dense(256)(outputs)
         model = tf.keras.Model(inputs=sequence_input, outputs=outputs)
         crf_model = CRFModel(model, 5)
-        crf_model.build(tf.TensorShape([None, 256]))
+        # crf_model.build(tf.TensorShape([None, 256]))
         return crf_model
 
     def test_build_model(self):
@@ -38,27 +23,6 @@ class CRFModelTest(unittest.TestCase):
         model.compile(optimizer='adam', metrics=['acc'])
         model.summary()
 
-    def test_train_model(self):
-        model = self._build_model()
-        model.compile(optimizer='adam', metrics=['acc'])
-
-        dataset, predict_dataset, labels = self._build_inputs()
-        model.fit(
-            dataset,
-            steps_per_epoch=100,
-            epochs=10,
-            validation_data=dataset,
-            callbacks=[
-                EvaluateCallback(token_mapper, label_mapper, os.path.join(DATADIR, 'example.dev')),
-            ])
-
-        preds = model.predict(predict_dataset, steps=1)
-        for idx, p in enumerate(preds):
-            # print(p)
-            example = np.argmax(p, axis=-1).tolist()[:len(labels[idx])]
-            print(labels[idx])
-            print(label_mapper.decode(example))
-            print()
 
 if __name__ == "__main__":
     unittest.main()
