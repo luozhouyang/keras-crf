34b8a1231d53804fe87432f8ef9a2af6194df960
luozhouyang <zhouyang.luo@gmail.com>
2021-08-02 17:48:36 +0800

    Remove files

--- keras_crf/__init__.py
+++ keras_crf/__init__.py
@@ -1,7 +1,4 @@
-from .callbacks import EvaluateCallback
 from .crf_model import CRFModel
-from .dataset import (ChinaPeopleDailyBuilder, DatasetBuilder, LabelMapper,
-                      TokenMapper)
 
 __name__ = 'keras_crf'
 __version__ = '0.2.0'
